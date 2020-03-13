6a179898104bfa72548decb778f50d975f2e6e51
sushchikh <a.sushchikh@gmail.com>
2020-03-13 16:51:00 +0300

    add color scheme for markers

--- py_raw/map.py
+++ py_raw/map.py
@@ -12,10 +12,25 @@ folium_map = folium.Map(location=[58.584884, 49.6247921],
                         zoom_start=13
                         )
 
+def money_scale_definder(money):
+    return {
+        0 < money < 3000: '1',
+        3000 < money < 6000: '2',
+        6000 < money < 10000: '3',
+        10_000 < money < 15_000: '4',
+        15_000 < money < 20_000: '5',
+        20_000 < money: '6'
+    }[True]
+
+# print(monyey_scale_definder(33500))
+
+
 for i in tqdm(range(len(address_df['address']))):
     lat = address_df['lat'][i]
     long = address_df['long'][i]
-    tooltip = address_df['name'][i]
-    folium.Marker([lat, long], popup=f'<i>{tooltip}</i>', tooltip=tooltip, icon=folium.CustomIcon( icon_image='https://imgur.com/Rzs4Zpa.png', icon_size=(8,8) )).add_to(folium_map)
+    tooltip = str(address_df['name'][i]) + "   " + str(round(address_df['money'][i], 0)) + ' p.'
+    marker_color = './../img/' + str(money_scale_definder(address_df['money'][i])) + '.png'
+    print(marker_color)
+    folium.Marker([lat, long], popup=f'<i>{tooltip}</i>', tooltip=tooltip, icon=folium.CustomIcon( icon_image=marker_color, icon_size=(8,8) )).add_to(folium_map)
 
 folium_map.save('./../map/kirov_map.html')
--- py_raw/work_with_data.py
+++ py_raw/work_with_data.py
@@ -4,9 +4,11 @@ import pandas as pd
 
 
 
+# raw data, without coordinates
+address_df = pd.read_excel('./../dats/strbt_orp.xls', sheet_name='Sheet1')
 
-address_df = pd.read_excel('./../dats/strbt_orp.xls', sheet_name='Sheet1')  # raw data, without coordinates
-address_df['url'] = ['https://www.google.com/maps/search/' + i for i in address_df['address']]  # add search-request
+# add search-request column to datafraim:
+address_df['url'] = ['https://www.google.com/maps/search/' + i for i in address_df['address']]
 
 # data, that we already taken:
 addresses_that_have_already_been_taken_df = pd.read_excel('./../dats/address.xls', sheet_name='address')
@@ -23,14 +25,20 @@ print('merged data: ', len(merged_df))
 
 driver = webdriver.Chrome()
 for i in tqdm(range(len(merged_df['name']))):
-    if not(pd.isnull(merged_df['long'][i])):
-        print('этот пользователь уже был', merged_df['name'][i])
-        continue
-    driver.get(merged_df['url'][i])
-    print('a этого пользователя еще не было:', merged_df['name'][i])
-    url_with_coordinates = (driver.find_element_by_css_selector('meta[itemprop=image]').get_attribute('content'))
-    merged_df['lat'][i] = (url_with_coordinates.split('?center=')[1].split('&zoom')[0].split('%2C')[0])
-    merged_df['long'][i] = (url_with_coordinates.split('?center=')[1].split('&zoom')[0].split('%2C')[1])
+    try:
+        if not(pd.isnull(merged_df['long'][i])):
+            print('этот пользователь уже был', merged_df['name'][i])
+            continue
+        driver.get(merged_df['url'][i])
+        print('a этого пользователя еще не было:', merged_df['name'][i])
+        url_with_coordinates = (driver.find_element_by_css_selector('meta[itemprop=image]').get_attribute('content'))
+        merged_df['lat'][i] = (url_with_coordinates.split('?center=')[1].split('&zoom')[0].split('%2C')[0])
+        merged_df['long'][i] = (url_with_coordinates.split('?center=')[1].split('&zoom')[0].split('%2C')[1])
+    except IndexError:
+        print('оп, ошибка, адрес не нашелся')
+        merged_df['lat'][i] = 0
+        merged_df['lat'][i] = 0
+
 
 writer = pd.ExcelWriter('./../dats/address.xls', engine='xlsxwriter')
 merged_df.to_excel(writer, sheet_name='address', index=False)
